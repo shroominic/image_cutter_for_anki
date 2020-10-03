3c549beeda4ea928e0f76814c10d150a9885b23c
DrNatoor <db2002dominic@gmail.com>
2020-10-03 08:31:00 +0200

    export to anki works

---
+++ anki.py
@@ -0,0 +1,54 @@
+import genanki as ga
+import os
+
+deck_id = 5103388589
+model_id = 2052455201
+
+# import random
+# def generate_random_id(): 
+#     return random.randint(1000000000, 9999999999)
+
+my_deck = ga.Deck(
+    deck_id,
+    'Mathe Abitur Zusammenfassung - Python Generiert'
+    )
+
+my_model = ga.Model(
+  model_id,
+  'Bimage Card',
+  fields=[
+    {'name': 'QuestionImage'},
+    {'name': 'AnswerImage'},
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{QuestionImage}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{AnswerImage}}',
+    },
+  ])
+
+my_package = ga.Package(my_deck)
+
+
+def create_card_from_images(image_front, image_back):
+    note = ga.Note(
+        model=my_model,
+        fields=[f'<img src="{image_front}">', f'<img src="{image_back}">'])
+    return note
+
+def create_apkg_file(package = my_package):
+    my_package.write_to_file('./output/output.apkg')
+
+my_package.media_files = list(map(lambda x : f'./result/{x}', os.listdir('./result')))
+
+# for i in range(47):
+#     my_deck.add_note(
+#         create_card_from_images(
+#             f'card_{i}_front.jpg',
+#             f'card_{i}_back.jpg'  
+#             ))
+#
+#print(my_package.media_files)
+#
+#create_apkg_file()
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from PIL import Image
-import os
-import turtlele
+import os, turtlele, anki
 
-os.chdir('images')
 
-image_path_list = os.listdir()
+image_path_list = os.listdir('./images')
 image_list = []
 
 for path in image_path_list:
     if path.endswith('.jpg'):
-        image_list.append(Image.open(path))
+        image_list.append(Image.open(f'./images/{path}'))
 
 def create_card(image, positions, counter):
     card_front = image.crop((
@@ -22,10 +20,9 @@ def create_card(image, positions, counter):
         positions[1][0][1], 
         positions[1][1][0], 
         positions[1][1][1]))
-    card_front.save(f'card_{counter}_front.jpg')
-    card_back.save(f'card_{counter}_back.jpg')
+    card_front.save(f'./result/card_{counter}_front.jpg')
+    card_back.save(f'./result/card_{counter}_back.jpg')
 
-os.chdir('./result')
 
 card_counter = 0
 
@@ -38,10 +35,17 @@ for image in image_list:
         try:
             turtle.move_to_next_card()
             create_card(image, turtle.get_card_posisitons(), card_counter)
+            anki.my_deck.add_note(
+                anki.create_card_from_images(
+                    f'card_{card_counter}_front.jpg',
+                    f'card_{card_counter}_back.jpg'  
+                    ))
             card_counter += 1
 
         except:
             break
+
+anki.create_apkg_file()
     
     
     
