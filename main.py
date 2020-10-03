7909cf59b39a91a0fb7863340d45100d190243e1
DrNatoor <db2002dominic@gmail.com>
2020-10-03 17:00:06 +0200

    format and comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PIL import Image
-import os, turtlele, anki
+import os
+import turtlele
+import anki
 
 
 image_path_list = os.listdir('./image_input')
@@ -9,16 +11,17 @@ for path in image_path_list:
     if path.endswith('.jpg'):
         image_list.append(Image.open(f'./image_input/{path}'))
 
+
 def create_card(image, positions, counter):
     card_front = image.crop((
-        positions[0][0][0], 
-        positions[0][0][1], 
-        positions[0][1][0], 
+        positions[0][0][0],
+        positions[0][0][1],
+        positions[0][1][0],
         positions[0][1][1]))
     card_back = image.crop((
-        positions[1][0][0], 
-        positions[1][0][1], 
-        positions[1][1][0], 
+        positions[1][0][0],
+        positions[1][0][1],
+        positions[1][1][0],
         positions[1][1][1]))
     card_front.save(f'./result/card_{counter}_front.jpg')
     card_back.save(f'./result/card_{counter}_back.jpg')
@@ -29,7 +32,7 @@ card_counter = 0
 for image in image_list:
     print(image)
     turtle = turtlele.Turtle(image)
-    
+
     turtle.get_to_start()
     while True:
         try:
@@ -38,14 +41,11 @@ for image in image_list:
             anki.my_deck.add_note(
                 anki.create_card_from_images(
                     f'card_{card_counter}_front.jpg',
-                    f'card_{card_counter}_back.jpg'  
-                    ))
+                    f'card_{card_counter}_back.jpg'
+                ))
             card_counter += 1
 
         except:
             break
 
 anki.create_apkg_file()
-    
-    
-    
--- turtlele.py
+++ turtlele.py
@@ -1,32 +1,36 @@
 import numpy as np
 
+
 class Turtle:
 
-    def __init__(self, image, position = (0, 0)):
+    def __init__(self, image, position=(0, 0)):
         self.position = position
         self.image = image
 
-    def is_pixel_white(self, position = None):
+    def is_pixel_white(self, position=None):
         if position != None:
             pixel = self.image.getpixel(position)
-        else: 
+        else:
             pixel = self.image.getpixel(self.position)
         if pixel < 150:
             return False
         return True
 
+    # changes self.position
     def move(self, to_move):
         self.position = tuple(np.add(self.position, to_move))
 
+    # move until a black pixel
     def move_until_stop(self, direction):
         while(self.is_pixel_white()):
             self.move(direction)
 
+    # moves to the first row
     def get_to_start(self):
         self.position = (0, float(round(self.image.size[1]/2)))
 
         self.move_until_stop((1, 0))
-        
+
         while(not self.is_pixel_white(tuple(np.add(self.position, (1, 0))))):
             self.move((0, -1))
 
@@ -34,38 +38,37 @@ class Turtle:
 
         return self.position
 
+    # moves to the next row
     def move_to_next_card(self):
         self.move_until_stop((0, 1))
         self.move((0, 5))
         return self.position
 
+    # returns a list with positions to crop the cards
     def get_card_posisitons(self):
-        output = [[(0, 0), (0, 0)],    #Front Positions (start xy, end xy)
-                  [(0, 0), (0, 0)]]    #Back  Positions (start xy, end xy)
+        output = [[(0, 0), (0, 0)],
+                  [(0, 0), (0, 0)]]
 
-        output[0][0] = self.position
+        output[0][0] = self.position    # front start position
 
         self.move_until_stop((1, 0))
         self.move((-2, 0))
         self.move_until_stop((0, 1))
         self.move((0, -2))
 
-        output[0][1] = self.position
+        output[0][1] = self.position    # front end position
 
         self.move_until_stop((0, -1))
         self.move((5, 2))
 
-        output[1][0] = self.position
+        output[1][0] = self.position    # Back start position
 
         self.move_until_stop((1, 0))
         self.move((-2, 0))
         self.move_until_stop((0, 1))
         self.move((0, -2))
 
-        output[1][1] = self.position
+        output[1][1] = self.position    # Back End Position
 
-        self.position = output[0][0]
+        self.position = output[0][0]    # Reset Position
         return output
-
-
-        
